560de1fb8510089fea59aaf4555953ded786fb4a
MR-Addcit <2750417853@qq.com>
2022-03-12 10:10:38 +0800

    Add lable text to html

--- Door lock system/Code/RFID/data/index.js
+++ Door lock system/Code/RFID/data/index.js
@@ -16,11 +16,11 @@ websocket.onmessage = function (event) {
 
 // websocket events
 function onOpen(event) {
+  document.getElementById("labelText").innerHTML = "Finished";
   console.log("Server Connected!");
 }
 function onClose(event) {
   console.log("Server Disconnected!");
-  alert("Server Disconnected!");
 }
 function onError(event) {
   console.log("Error:" + event.data);
@@ -30,12 +30,19 @@ function onMessage(event) {
   var JSONContent = JSON.parse(event.data);
   if (JSONContent.hasOwnProperty("Door")) {
     document.getElementById("openButton").style.backgroundColor = "white";
+    document.getElementById("labelText").innerHTML = "Finished!";
   }
 }
 
 // Open Door
 function openDoor(element) {
+  if (
+    document.getElementById("labelText").innerHTML === "Please wait for response" ||
+    document.getElementById("labelText").innerHTML === "Opening..."
+  )
+    return;
   element.style.backgroundColor = "salmon";
+  document.getElementById("labelText").innerHTML = "Opening...";
   var command = '{"Door":"Open"}';
   websocket.send(command);
   console.log(command);
