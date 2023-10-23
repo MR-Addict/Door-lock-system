ff834a6cfaeea2a5202e81b964d222cf8f223b79
MR-Addict <MR-Addict@qq.com>
2023-10-23 14:29:12 +0800

    feat: simplify code

--- README.md
+++ README.md
@@ -3,15 +3,14 @@
 This NFC door lock system uses ESP32 and 28BYJ-48 stepper motors.
 
 ## 1. Add a new card
+
 You can use `RFID_READ.ino` file to read a new card, the formate should be like this `{0xF1, 0x41, 0x01, 0x5F}`.
 
 Then add new card to `RFID.ino` file.
 
 ```cpp
 const uint8_t UID[][4] = {
-    {0xF1, 0x41, 0x01, 0x5F}, {0x01, 0x5E, 0x95, 0x2B}, {0x1A, 0xFD, 0xA5, 0x21},
-    {0xD1, 0x7E, 0xFF, 0x5E}, {0x21, 0xDE, 0x01, 0x5F}, {0xD1, 0x3A, 0xF6, 0x5E},
-    {0x0A, 0x57, 0x80, 0x1C}, {0xFA, 0x5D, 0x70, 0x7F}, {0xD2, 0xAA, 0x1C, 0x3E},
+    {0xF1, 0x41, 0x01, 0x5F},
 };
 ```
 
@@ -21,28 +20,12 @@ In order to make this system more efficent, I add a sensor to interrupt MCU. So 
 
 Besides RFID card access, you can also use web UI by [http://unlockdoor-507](http://unlockdoor-507) if you connect the same router.
 
-You need to login in first, the default username is `admin`, default password is `dinghao666`.
+You need to login in first, the default username is `admin`, default password is `stas`.
 
 Login page
 
-![Login](images/login.png)
+![login](images/login.png)
 
-Web UI page
+Home page
 
-![Login](images/home.png)
-
-## 3. Change configurations
-
-You can hold boot button on ESP32 for 2 seconds, and you will enter into WiFi manager mode where you can change some basic settings for your device.
-
-After you press button for 2 seconds, ESP32 will enter ino AP mode, and generated an `Access Point` whose ssid is `WiFi Manger` and the password is `123456789` and the default gateway is [http://192.168.4.1](http://192.168.4.1)
-
-![Manger](images/manager.png)
-
-## 4. Upload bin through Web
-
-You can also upload bin file directly by web page on [https://unlockdoor-507/upload](https://unlockdoor-507/upload).
-
-![upload](images/update.png)
-
-Happy Hacking!
+![home](images/home.png)
---
+++ code/RFID_ESP32_With_NFC/RFID_ESP32_With_NFC.ino
@@ -0,0 +1,115 @@
+// Header files for NFC
+#include <SPI.h>
+#include <MFRC522.h>
+#include <AccelStepper.h>
+
+// Header files for WIFI
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <ArduinoJson.h>
+#include <ESPAsyncWebServer.h>
+
+const uint8_t PinA = 14;
+const uint8_t PinB = 27;
+const uint8_t PinC = 26;
+const uint8_t PinD = 25;
+const uint8_t Buzzer = 12;
+const uint8_t SS_PIN = 5;
+const uint8_t RST_PIN = 22;
+const uint8_t INT_PIN = 4;
+
+const String LOGIN_USER = "admin";
+const String LOGIN_PASSWORD = "stas";
+const String WIFI_SSID = "STAS-507";
+const String WIFI_PASSWORD = "manke666";
+
+volatile boolean isOpenDoor = false;
+volatile boolean isDetectedCard = false;
+const int16_t stepsPerRevolution = -500;
+
+const uint8_t UID[][4] = {
+  {0xF1, 0x41, 0x01, 0x5F},
+};
+
+MFRC522 rfid = MFRC522(SS_PIN, RST_PIN);
+AccelStepper stepper(8, PinA, PinC, PinB, PinD);
+AsyncWebServer server(80);
+
+void ringBuzzer(uint8_t);
+void UID_ISR();
+bool checkUID();
+void openDoor();
+
+// Header files for custom function
+#include "WIFI_WS.h"
+
+void setup() {
+  SPI.begin();
+  Serial.begin(115200);
+  pinMode(Buzzer, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(INT_PIN), UID_ISR, FALLING);
+  rfid.PCD_Init();
+
+  stepper.setMaxSpeed(500.0);
+  stepper.setAcceleration(1000.0);
+  stepper.runToNewPosition(0);
+
+  WIFI_Init();
+  Server_Init();
+
+  ringBuzzer(100);
+}
+
+void loop() {
+  if (isOpenDoor)openDoor();
+  if (isDetectedCard)readCard();
+}
+
+void ringBuzzer(uint8_t duration) {
+  for (uint8_t i = 0; i < 2; i++) {
+    digitalWrite(Buzzer, HIGH);
+    delay(duration);
+    digitalWrite(Buzzer, LOW);
+    delay(100);
+  }
+}
+
+void UID_ISR() {
+  isDetectedCard = true;
+}
+
+bool checkUID() {
+  for (uint8_t i = 0; i < sizeof(UID) / (sizeof(uint8_t) * 4); i++) {
+    for (uint8_t j = 0, k = 0; j < 4; j++) {
+      if (rfid.uid.uidByte[j] != UID[i][j])k++;
+      if (!k)return true;
+    }
+  }
+  return false;
+}
+
+void readCard() {
+  if (!isDetectedCard)return;
+  delay(300);
+  if (!rfid.PICC_IsNewCardPresent())return;
+  if (!rfid.PICC_ReadCardSerial())return;
+
+  if (checkUID()) {
+    isOpenDoor = true;
+  }
+  else {
+    ringBuzzer(200);
+  }
+
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+  isDetectedCard = false;
+}
+
+void openDoor() {
+  ringBuzzer(100);
+  stepper.runToNewPosition(stepsPerRevolution);
+  delay(1000);
+  stepper.runToNewPosition(0);
+  isOpenDoor = false;
+}
